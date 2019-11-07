6afb0c7f4a1870f5102d56eef726f01408526d5c
edualb <eduardoslafiq@gmail.com>
2019-11-07 18:23:10 -0300

    FooterView implemented

--- globoplay-quiz/Components/FooterView/FooterView.swift
+++ globoplay-quiz/Components/FooterView/FooterView.swift
@@ -11,16 +11,29 @@ import UIKit
 final class FooterView: UIView {
     
     private let footerImageView: UIImageView = {
-       let imageView = UIImageView(image: UIImage(named: "footer-wave"))
+        let imageView = UIImageView(image: UIImage(named: "footer-wave"))
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.contentMode = .scaleAspectFill
         imageView.backgroundColor = Color.darkGray
         return imageView
     }()
     
+    private let nextButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Próximo", for: .normal)
+        button.setTitleColor(Color.white, for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20.0)
+        button.contentEdgeInsets = .init(top: 0, left: 38, bottom: 0, right: 38)
+        button.layer.cornerRadius = 25
+        button.backgroundColor = Color.black
+        return button
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.addSubview(footerImageView)
+        self.addSubview(nextButton)
         self.setupAnchor()
     }
     
@@ -34,5 +47,10 @@ final class FooterView: UIView {
         self.footerImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
         self.footerImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
         self.footerImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+        
+        //MARK: - NextButton Anchor
+        self.nextButton.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        self.nextButton.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -14).isActive = true
+        self.nextButton.heightAnchor.constraint(equalToConstant: 51).isActive = true
     }
 }
